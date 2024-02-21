f0afb809bc85f0ef2926a7e54d7dd4967b36ab03
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-02-21 22:06:13 +0530

    Rotation Bug Fix

--- Assets/Scripts/AutoRotator.cs
+++ Assets/Scripts/AutoRotator.cs
@@ -20,14 +20,31 @@ public class AutoRotator : MonoBehaviour
     private void Update()
     {
         if(!isAutoRotating) return;
+        Vector3 ra = new Vector3(1,0,0);
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            int roll_Z = Mathf.RoundToInt(transform.rotation.eulerAngles.z / perSectionAngle);
-            int newRotation = roll_Z * perSectionAngle;
+            int roll_Z = Mathf.RoundToInt(Ang(transform.rotation.eulerAngles.z - offset) / perSectionAngle);
+            int newRotation = roll_Z * (perSectionAngle);
 
-            newRot = Quaternion.AngleAxis(newRotation + offset, Vector3.forward);          
+            newRot = Quaternion.AngleAxis(newRotation + offset, Vector3.forward);
         }
+        //Debug.Log(Vector3.Angle(transform.right, ra)); 
+        Debug.Log(transform.rotation.eulerAngles.z);
         transform.rotation = Quaternion.Slerp(transform.rotation, newRot, 10f * Time.deltaTime);
+    }
+    private float Ang(float a)
+    {
+        if (a >= 180)
+        {
+            return a - 360;
+        }
+        return a;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(Vector3.zero, Vector3.right * 10);
     }
 }
